770fbc74271cbdcc6a82238c452e13413a00daae
Deepak Velmurugan <deepazlions@gmail.com>
2021-02-09 11:49:10 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ classifiers = [
  
 setup(
   name='deepNets',
-  version='0.1.4',
+  version='0.1.6',
   description='A basic deep learning tool',
   long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
   url='',  
